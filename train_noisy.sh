707ee95df7fcd21bb7d8d2e54d90f26ab45f4c71
guo <48042521+douling843@users.noreply.github.com>
2026-02-07 21:06:35 +0800

    Add files via upload

---
+++ train_noisy.sh
@@ -0,0 +1,50 @@
+
+
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_10_5.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_10_5' 
+
+
+
+
+
+
+
+
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_OAMIL_10_5.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_OAMIL_10_5' 
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_01_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_01_5_mask2obb_MIL' 
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_02_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_02_5_mask2obb_MIL' 
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_03_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_03_5_mask2obb_MIL' 
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_04_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_04_5_mask2obb_MIL' 
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_01_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_01_5_mask2obb_MIL_Riou' 
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_02_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_02_5_mask2obb_MIL_Riou' 
+
+
+# CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_03_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_03_5_mask2obb_MIL_Riou' 
+
+
+CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/rotated_faster_rcnn/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_04_5_mask2obb_MIL.py --work-dir='work_dir/ssdd/rotated_faster_rcnn_r50_fpn_3x_ssdd_le90_04_5_mask2obb_MIL_Riou' 
+
+
+
+
+
+
+
+
+
+
+
+
